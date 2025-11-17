25d5800a559af797e4a7e11557034962c322eefa
alcalino-git <alanhumber333@gmail.com>
2025-11-17 12:25:41 -0600

    Added data editing

--- lib/widgets/timeline_edit.dart
+++ lib/widgets/timeline_edit.dart
@@ -3,29 +3,51 @@ import 'dart:convert';
 import 'package:csv/csv.dart';
 import 'package:flutter/material.dart';
 import 'package:timeliner_flitter/logic/state.dart';
+import 'package:editable/editable.dart';
 
 class _TimelineEditState extends State<TimelineEditWidget> {
   final state = AppState();
+  final _editableKey = GlobalKey<EditableState>(); 
+  List<List<dynamic>> data = const CsvToListConverter().convert(
+    AppState().file?.readAsStringSync(encoding: utf8),
+  );
+
+
+  void _saveState() {
+    String csv = ListToCsvConverter().convert(data);
+    state.file!.writeAsStringSync(csv); 
+  }
+
+
+  void _handleEditedRow() {
+        var edited = (_editableKey.currentState!.editedRows);
+        setState(() {
+          for (var (row as Map) in edited) {
+            var rowIndex = row['row']+1;
+            var editedHeaders = row.keys.where((v) {return v != 'row';}).toList();
+            for (var h in editedHeaders) {
+              data[rowIndex][data[0].indexOf(h)] = row[h];
+            }
+          }
+
+          _saveState();
+        });
+        
+  }
 
   Widget buildView() {
-    
-    List<List<dynamic>> data = const CsvToListConverter().convert(
-      state.file?.readAsStringSync(encoding: utf8),
-    );
-    var headers = data[0];
 
-    return DataTable(
-          columns: headers.map((h) {
-            return DataColumn(label: Text(h));
-          }).toList(),
-          rows: data.sublist(1).map((r) {
-            return DataRow(
-              cells: r.map((v) {
-                return DataCell(Text(v));
-              }).toList(),
-            );
-          }).toList(),
-        );
+    
+    return Editable(
+      onSubmitted: (_) => _handleEditedRow(),
+      key: _editableKey,
+      columns: data[0].map((e) {
+        return {"title":e, 'widthFactor': 0.2, 'key':e};
+      }).toList(),
+      rows: data.sublist(1).map((r) {
+        return Map.fromIterables(data[0], r);
+      }).toList(),
+    );
   }
 
   @override

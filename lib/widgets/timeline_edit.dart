3bc5e08bd99c79342f29282c97381ffb12f98596
alcalino-git <alanhumber333@gmail.com>
2025-11-17 22:18:06 -0600

    Made edit tab functionable

--- lib/widgets/timeline_edit.dart
+++ lib/widgets/timeline_edit.dart
@@ -7,57 +7,102 @@ import 'package:editable/editable.dart';
 
 class _TimelineEditState extends State<TimelineEditWidget> {
   final state = AppState();
-  final _editableKey = GlobalKey<EditableState>(); 
+  final _editableKey = GlobalKey<EditableState>();
+
   List<List<dynamic>> data = const CsvToListConverter().convert(
     AppState().file?.readAsStringSync(encoding: utf8),
   );
 
-
   void _saveState() {
     String csv = ListToCsvConverter().convert(data);
-    state.file!.writeAsStringSync(csv); 
+    state.file!.writeAsStringSync(csv);
   }
 
+  void _handleDeleteRow() {
+    setState(() {
+      data = data.sublist(0, data.length - 1).toList();
+    });
+
+    _saveState();
+
+    _editableKey.currentState?.rows = _editableKey.currentState!.rows!.sublist(
+      0,
+      _editableKey.currentState!.rows!.length - 1,
+    );
+    setState(() {});
+  }
+
+  void _handleAddRow() {
+    data.add(
+      List.generate(data[0].length, (e) {
+        return "";
+      }),
+    );
+    _editableKey.currentState!.createRow();
+    _saveState();
+    setState(() {});
+  }
 
   void _handleEditedRow() {
-        var edited = (_editableKey.currentState!.editedRows);
-        setState(() {
-          for (var (row as Map) in edited) {
-            var rowIndex = row['row']+1;
-            var editedHeaders = row.keys.where((v) {return v != 'row';}).toList();
-            for (var h in editedHeaders) {
-              data[rowIndex][data[0].indexOf(h)] = row[h];
-            }
-          }
+    var edited = (_editableKey.currentState!.editedRows);
 
-          _saveState();
-        });
-        
+    for (var r in edited) {
+      var row = r as Map;
+      var rowIndex = row['row'] + 1;
+      if (rowIndex >= data.length) {
+        data.add(
+          List.generate(data[0].length, (e) {
+            return "";
+          }),
+        );
+      }
+      var editedHeaders = row.keys.where((v) {
+        return v != 'row';
+      }).toList();
+      for (var h in editedHeaders) {
+        data[rowIndex][data[0].indexOf(h)] = row[h];
+      }
+    }
+
+    setState(() {});
+    _saveState();
   }
 
   Widget buildView() {
+    return IntrinsicHeight(
+      child: Editable(
+        onSubmitted: (_) => _handleEditedRow(),
 
-    
-    return Editable(
-      onSubmitted: (_) => _handleEditedRow(),
-      key: _editableKey,
-      columns: data[0].map((e) {
-        return {"title":e, 'widthFactor': 0.2, 'key':e};
-      }).toList(),
-      rows: data.sublist(1).map((r) {
-        return Map.fromIterables(data[0], r);
-      }).toList(),
+        key: _editableKey,
+        columns: data[0].map((e) {
+          return {"title": e, 'widthFactor': 0.17, 'key': e};
+        }).toList(),
+        rows: data.sublist(1).map((r) {
+          return Map.fromIterables(data[0], r);
+        }).toList(),
+      ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
-
-    return StreamBuilder(
-      stream: state.watcher,
-      builder: (context, snapshot) {
-        return buildView();
-      },
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        StreamBuilder(
+          stream: state.watcher,
+          builder: (context, snapshot) {
+            return buildView();
+          },
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(onPressed: () {_handleAddRow();}, child: Text("Add row")),
+            TextButton(onPressed: () {_handleDeleteRow();}, child: Text("Delete row"))
+          ],
+        )
+      ],
     );
   }
 }
